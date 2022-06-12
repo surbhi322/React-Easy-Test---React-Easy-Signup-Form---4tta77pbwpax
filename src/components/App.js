0598e5ca4c8ef4b797133a013b4ba745c0305289
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-06-12 21:56:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React from "react";
+import SignUp from './SignUp';
 
 const App = () => {
-  
   return (
-
+    <div className ="App">
+        <SignUp />
+    </div>
+    
   )
 }
-
-
 export default App;
