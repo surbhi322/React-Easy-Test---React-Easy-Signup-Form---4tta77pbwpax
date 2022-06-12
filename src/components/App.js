3c02215f5150bde374f9456b8f88c19f43815d8e
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-06-12 22:28:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,90 +1,13 @@
-import React, { useState } from "react";
-import "../styles/App.css";
-import { signUpFormValidation } from "../utils/validation";
+import React from "react";
+import SignUp from './SignUp';
+
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [consent, setConsent] = useState(false);
-  const [errors, setErrors] = useState({});
   return (
-    <>
-      <form
-        onSubmit={(e) => {
-          // console.log(name);
-          let obj = {
-            name: name,
-            email: email,
-            password: password,
-            consent: consent
-          };
-
-          e.preventDefault();
-          setErrors(signUpFormValidation(obj));
-          //console.log(errors);
-        }}
-      >
-        <div>
-          <label>
-            Name
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            ></input>
-          </label>
-          <p>{errors.name ? errors.name : null}</p>
-        </div>
-        <div>
-          <label>
-            Email
-            <input
-              type="text"
-              value={email}
-              id="email"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            ></input>
-          </label>
-          <p>{errors.email ? errors.email : null}</p>
-        </div>
-        <div>
-          <label>
-            Password
-            <input
-              type="PassWord"
-              id="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            ></input>
-          </label>
-          <p>{errors.password ? errors.password : null}</p>
-        </div>
-        <div>
-          <label>
-            Consent
-            <input
-              type="checkbox"
-              id="consent"
-              required
-              onChange={() => {
-                setConsent(!consent);
-              }}
-            ></input>
-          </label>
-          {/* <p>{!consent ? "Checkbox not selected" : null}</p> */}
-        </div>
-        <button type="submit">SignUp</button>
-      </form>
-    </>
-  );
-};
-
+    <div className ="App">
+        <SignUp />
+    </div>
+    
+  )
+}
 export default App;
